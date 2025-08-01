a08e3972c888863be7f416117808e9b6644f7f34
Alexandernacato <alexandernacato@gmail.com>
2025-07-31 22:55:34 -0500

    Actualizacion

---
+++ prueba/src/main/java/com/example/prueba/prueba/Client/CategoryDTO.java
@@ -0,0 +1,26 @@
+package com.example.prueba.prueba.Client;
+
+public class CategoryDTO {
+    private Long id;
+    private String nombre;
+
+    public CategoryDTO() {
+    }
+    public CategoryDTO(Long id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
